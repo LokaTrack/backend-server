2bb6797418eefe6dbd9c6c36b29da217fd299697
Cornelius Yuli R <145887908+cornel-yr@users.noreply.github.com>
2025-04-04 19:15:06 +0700

    update: add token payload to include additional user information

--- app/utils/security.py
+++ app/utils/security.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-
 # Password hashing
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -28,22 +27,24 @@ def createAccessToken(data: dict, expires_delta: Optional[timedelta] = None):
     """Create a JWT access token"""
     # dataToEncode = data.copy()
 
-    dataToEncode = {
-        "userId": data["userId"],
-        "email": data["email"],
-        "role": data["role"],
-        "username": data["username"]
-    }
-
     # if expires_delta is provided, use it
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         # data from .env always in string format
-        expired_days = int(os.getenv("ACCESS_TOKEN_EXPIRED_DAYS"), 30)  # Default to 30 days if not set
+        expired_days = int(os.getenv("ACCESS_TOKEN_EXPIRED_DAYS", "30"))  # Default to 30 days if not set
         expire = datetime.utcnow() + timedelta(days=expired_days)
-        
-    dataToEncode.update({"exp": expire})
+    
+    issuer = os.getenv("APP_DOMAIN") or "lokatrack"
+    dataToEncode = {
+        "userId": data["userId"],
+        "email": data["email"],
+        "role": data["role"],
+        "username": data["username"],
+        "exp": expire,
+        "iss": issuer
+    }
+
     encoded_jwt = jwt.encode(dataToEncode, os.getenv("SECRET_KEY"), os.getenv("ALGORITHM"))
     return encoded_jwt
 
